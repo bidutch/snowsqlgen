d927485d7f6dfdc9664f5711f4fb846f8aa06604
Johan-GitHub-Desktop <11424878+bidutch@users.noreply.github.com>
2021-01-14 20:20:22 +0100

    trying

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -1,3 +1,17 @@
 database = input(f'\nEnter the name of the database you want to create: ')
+environment = input(f'\nEnter the name of the environment (DTAP) you want to create: ')
 
-print('CREATE DATABASE ' + str(database))
+# Composing the code
+
+line1 = ('CREATE ROLE IF NOT EXISTS RL_' + str(database) + '_' + str(environment)+'_ADMIN;')
+line2 = ('USE ROLE RL_' + str(database) + '_' + str(environment)+'_ADMIN;')
+line3 = ('CREATE DATABASE DB_' + str(database) + '_' + str(environment)+';')
+
+with open('..\output\V1__createdatabase.sql','w') as out:
+    out.write('{}\n{}\n{}\n'.format(line1,line2,line3))
+  
+# Checking if the data is 
+# written to file or not 
+file1 = open('..\output\V1__createdatabase.sql', 'r') 
+print(file1.read()) 
+file1.close() 
