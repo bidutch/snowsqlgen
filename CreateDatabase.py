a4cc285d8a92200566e3d7128cc81bac9e7a6906
Johan van der Kooij <johanvanderkooij@hotmail.com>
2021-01-15 08:22:33 +0100

    various changes

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -6,17 +6,18 @@ environment = input(f'\nEnter the name of the environment (DTAP) you want to cre
 # Composing the code
 
 line10 = ('CREATE ROLE IF NOT EXISTS RL_' + str(database) + '_' + str(environment)+'_ADMIN;')
+line13 = ('GRANT ALL PRIVILEGES ON ACCOUNT TO ROLE RL_' + str(database) + '_' + str(environment)+'_ADMIN;')
 line15 = ('GRANT ROLE RL_' + str(database) + '_' + str(environment)+'_ADMIN TO USER JOHAN;')
 line20 = ('USE ROLE RL_' + str(database) + '_' + str(environment)+'_ADMIN;')
 line30 = ('CREATE DATABASE DB_' + str(database) + '_' + str(environment)+';')
 
 # Writing the lines to file
-file1='V1__createdatabase.sql'
+file1='V1.1__createdatabase.sql'
 with open(file1,'w') as out:
-    out.write('{}\n{}\n{}\n'.format(line10,line15,line20,line30))
+    out.write('{}\n{}\n{}\n{}\n{}\n'.format(line10,line13,line15,line20,line30))
   
 # Checking if the data is 
 # written to file or not 
-file1 = open('V1__createdatabase.sql', 'r') 
+file1 = open('V1.1__createdatabase.sql', 'r') 
 print(file1.read()) 
 file1.close()
